e57c6656abf5e87c6c71c0259d613340df6fa3ec
tcrow <luowei31@gmail.com>
2018-08-22 09:42:16 +0800

    1

--- src/main/java/org/tcrow/mutiple/MultipleThreadPrint.java
+++ src/main/java/org/tcrow/mutiple/MultipleThreadPrint.java
@@ -4,50 +4,68 @@ import com.google.common.base.Stopwatch;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author tcrow.luo
- *         无线程调度，实现多线程顺序打印abc字符串
+ * 实现多线程顺序打印abc字符串
  */
 public class MultipleThreadPrint {
 
-    private static AtomicInteger i = new AtomicInteger(0);
+    /**
+     * 状态位因print方法已加锁，无需使用Atomic类提高处理效率
+     */
+    private static int i = 0;
 
-    private static StringBuffer validateSb = new StringBuffer();
+    /**
+     * 校验字符串因print方法已加锁，无需使用StringBuffer提高处理效率
+     */
+    private static StringBuilder validateSb = new StringBuilder();
 
     private final static int PRINT_TIMES = 50;
+    private final static int THREAD_NUM = 3;
 
     private static class Print implements Runnable {
-
         @Override
         public void run() {
             for (int j = 0; j < PRINT_TIMES; j++) {
                 print();
             }
-
         }
     }
 
-    private synchronized static void print() {
-        char c = (char) ('a' + i.getAndIncrement() % 3);
+    /**
+     * 方法加锁保证打印时其他线程不会抢先打印
+     *
+     * @return
+     */
+    private synchronized static char print() {
+        char c = (char) ('a' + i++ % 3);
         System.out.print(c);
         validateSb.append(c);
+        return c;
     }
 
     public static void main(String[] args) {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        ExecutorService pool = Executors.newFixedThreadPool(3);
-        pool.execute(new Print());
-        pool.execute(new Print());
-        pool.execute(new Print());
+        ExecutorService pool = Executors.newFixedThreadPool(THREAD_NUM);
+        for (int j = 0; j < THREAD_NUM; j++) {
+            pool.execute(new Print());
+        }
         pool.shutdown();
         while (true) {
-            if (pool.isTerminated()){
+            if (pool.isTerminated()) {
                 stopwatch.stop();
-                System.out.println("\n" + stopwatch.toString());
-                String correct = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc";
-                System.out.println(correct.equals(validateSb.toString()));
+                System.out.println("\n multiple thread spend time:" + stopwatch.toString());
+                StringBuilder correct = new StringBuilder();
+                stopwatch.reset();
+                stopwatch.start();
+                for (int j = 0; j < PRINT_TIMES * THREAD_NUM; j++) {
+                    char c = (char) ('a' + j % 3);
+                    correct.append(c);
+                    System.out.print(c);
+                }
+                System.out.println("\nsingle thread spend time:" + stopwatch.toString());
+                System.out.println(correct.toString().equals(validateSb.toString()));
                 break;
             }
         }
