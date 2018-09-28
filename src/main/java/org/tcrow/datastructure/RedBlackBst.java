48f97860ecfedde42950228a004b88e8431decd8
tcrow <luowei31@gmail.com>
2018-09-28 15:22:02 +0800

    [modify]RedBlackBst

--- src/main/java/org/tcrow/datastructure/RedBlackBst.java
+++ src/main/java/org/tcrow/datastructure/RedBlackBst.java
@@ -6,13 +6,13 @@ package org.tcrow.datastructure;
  */
 public class RedBlackBst<Key extends Comparable<Key>, Value> {
 
-    private RedBlackBst.Node root;
+    private Node root;
 
     private class Node {
         private Key key;
         private Value value;
         private boolean color = false;
-        private RedBlackBst.Node left, right;
+        private Node left, right;
         private int size;
 
         public Node(Key key, Value value, int size) {
@@ -22,4 +22,175 @@ public class RedBlackBst<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 向上取整
+     */
+    public Key ceiling(Key key) {
+        Node node = ceiling(root, key);
+        if (node == null) {
+            return null;
+        }
+        return node.key;
+    }
+
+    private RedBlackBst.Node ceiling(RedBlackBst.Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = node.key.compareTo(key);
+        if (cmp == 0) {
+            return node;
+        } else if (cmp < 0) {
+            return ceiling(node.right, key);
+        }
+        RedBlackBst.Node t = ceiling(node.left, key);
+        if (t != null) {
+            return t;
+        }
+        return node;
+    }
+
+    /**
+     * 向下取整
+     */
+    public Key floor(Key key) {
+        Node node = floor(root, key);
+        if (node == null) {
+            return null;
+        }
+        return node.key;
+    }
+
+    private Node floor(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = node.key.compareTo(key);
+        if (cmp == 0) {
+            return node;
+        } else if (cmp > 0) {
+            return floor(node.left, key);
+        }
+        Node t = floor(node.right, key);
+        if (t != null) {
+            return t;
+        }
+        return node;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = node.key.compareTo(key);
+        if (cmp == 0) {
+            return node.value;
+        } else if (cmp < 0) {
+            return get(node.right, key);
+        } else {
+            return get(node.left, key);
+        }
+    }
+
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    private RedBlackBst.Node put(RedBlackBst.Node node, Key key, Value value) {
+        if (node == null) {
+            return new RedBlackBst.Node(key, value, 1);
+        }
+        int cmp = node.key.compareTo(key);
+        if (cmp == 0) {
+            node.value = value;
+        } else if (cmp < 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.left = put(node.left, key, value);
+        }
+        node.size = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    /**
+     * 返回比key小的节点数量
+     * @param key
+     * @return
+     */
+    public int rank(Key key) {
+        return rank(root, key);
+    }
+
+    private int rank(RedBlackBst.Node node, Key key) {
+        if (node == null) {
+            return 0;
+        }
+        int cmp = node.key.compareTo(key);
+        if (cmp == 0) {
+            return size(node.left);
+        } else if (cmp < 0) {
+            return 1 + size(node.left) + rank(node.right, key);
+        }
+        return rank(node.left, key);
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(RedBlackBst.Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    private Node max(RedBlackBst.Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return max(node.right);
+    }
+
+    public Key max() {
+        return max(root.right).key;
+    }
+
+    /**
+     * 返回排名第k的节点
+     * @param k
+     * @return
+     */
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    private Node select(RedBlackBst.Node node, int k) {
+        if (node == null) {
+            return null;
+        }
+        int t = size(node.left);
+        if (t == k) {
+            return node;
+        } else if (t < k) {
+            return select(node.right, k - t - 1);
+        }
+        return select(node.left, k);
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(RedBlackBst.Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.size;
+    }
 }
