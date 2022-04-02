cf0636aaf4efab234c20166789e249e2811262a2
Crow <123@qq.com>
2022-04-03 02:35:11 +0800

    从一个整形数组中找到奇数个数的数字

---
+++ src/main/java/org/tcrow/bilibili/class01/EvenTimes.java
@@ -0,0 +1,50 @@
+package org.tcrow.bilibili.class01;
+
+/**
+ * 从一个整形数组中找到奇数个数的数字
+ */
+public class EvenTimes {
+
+    private static final int[] numbers = new int[]{1, 2, 2, 3, 3, 3, 3, 1, 1};
+    private static final int[] numbers2 = new int[]{1, 2, 2, 3, 3, 3, 1, 1, 1, 1, 1, 5};
+
+    /**
+     * 找到数组中的唯一奇数次的数字
+     *
+     * @param arr
+     */
+    private static void printOddTimesNum1(int[] arr) {
+        int eor = 0;
+        for (int cur : arr) {
+            eor ^= cur;
+        }
+        System.out.println(eor);
+    }
+
+    /**
+     * 找到数组中的唯二奇数次的两个
+     *
+     * @param arr
+     */
+    private static void printOddTimesNum2(int[] arr) {
+        int eor = 0;
+        for (int cur : arr) {
+            eor ^= cur;
+        }
+        // eor == a ^ b and eor != 0
+        // because eor is not zero ,so eor has a one
+        int rightOne = eor & (~eor + 1); // pick the right one
+        int onlyOne = 0;// eor'
+        for (int cur : arr) {
+            if ((cur & rightOne) == 0) {// must zero ,can't equal one
+                onlyOne ^= cur;
+            }
+        }
+        System.out.println(onlyOne + " " + (eor ^ onlyOne));
+    }
+
+    public static void main(String[] args) {
+        printOddTimesNum1(numbers);
+        printOddTimesNum2(numbers2);
+    }
+}
