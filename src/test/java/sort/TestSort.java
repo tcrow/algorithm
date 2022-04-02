063c169631b660f49abcf2b426be208ce8ec69e7
Crow <123@qq.com>
2022-04-03 03:42:45 +0800

    增加一个对数器

--- src/test/java/sort/TestSort.java
+++ src/test/java/sort/TestSort.java
@@ -8,6 +8,7 @@ import org.tcrow.datastructure.Trie;
 import org.tcrow.sort.Sort;
 import org.tcrow.util.RandomUtil;
 
+import java.lang.annotation.Repeatable;
 import java.math.BigDecimal;
 
 
@@ -20,7 +21,8 @@ public class TestSort {
 
     //    final Integer[] arr = {39, 12, 46, 42, 19, 32, 68, 58, 11, 11, 33, 70, 47, 40, 79, 37, 50, 60, 67, 37, 64, 66, 91, 41, 62, 87, 88, 71, 8, 22, 74, 27, 27, 90, 41, 24, 50, 53, 92, 42, 33, 52, 74, 90, 7, 58, 32, 20, 25, 85, 5, 53, 34, 29, 75, 61, 74, 1, 0, 14, 99, 42, 18, 57, 38, 80, 54, 2, 48, 28, 76, 27, 95, 7, 36, 36, 48, 87, 53, 4, 0, 86, 76, 41, 20, 90, 40, 88, 22, 29, 62, 9, 48, 22, 19, 11, 30, 30, 97, 82};
     final Comparable[] arr = gaussianRandom();
-    final int len = 1000;
+    //    final Integer[] arr = generateRandomArray(180);
+    final int len = 10000;
 
     /**
      * 生成符合正态分布的数组
@@ -31,6 +33,20 @@ public class TestSort {
         Comparable[] arr = new Comparable[len];
         for (int i = 0; i < len; i++) {
             arr[i] = new BigDecimal(getNumberInNormalDistribution(180, 10)).intValue();
+        }
+        return arr;
+    }
+
+    /**
+     * 生成等概率随机数组(会有负数)
+     *
+     * @return
+     */
+    private Integer[] generateRandomArray(int maxValue) {
+        Integer[] arr = new Integer[(int) ((len + 1) * Math.random())]; // 随机长度
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
         }
         return arr;
     }
